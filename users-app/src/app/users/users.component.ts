1bd0befb5a33849aed88be696a2a91af68ef34a7
synergy2411 <synergy2411@outlook.com>
2019-10-21 17:26:11 +0530

    day 01

--- users-app/src/app/users/users.component.ts
+++ users-app/src/app/users/users.component.ts
@@ -12,6 +12,11 @@ export class UsersComponent {
     income: 50000,
     isWorking: true,
     company: "Microsoft",
-    votes: 120
+    votes: 120,
+    image : "https://image.cnbcfm.com/api/v1/image/104891709-Bill_Gates_the_co-Founder.jpg?v=1558120888&w=1400&h=950"
+  }
+
+  onMoreInfo(user : any){
+    alert(`${user.firstName} is working with ${user.company}!!`);
   }
 }
